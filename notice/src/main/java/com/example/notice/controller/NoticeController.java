87801fe4a9505f0073352e5d9fac734aa0c413be
guswl197 <guswl197@ajou.ac.kr>
2021-11-01 20:42:31 +0900

    notice기능 Addnotice API 개발

---
+++ notice/src/main/java/com/example/notice/controller/NoticeController.java
@@ -0,0 +1,22 @@
+package com.example.notice.controller;
+
+import com.example.notice.entity.Notice;
+import com.example.notice.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class NoticeController {
+
+    @Autowired
+    private NoticeService noticeService;
+
+    //공지사항 작성
+    @PutMapping("/addnotice")
+    public Notice Addnotice(@RequestBody Notice notice){
+        return noticeService.write(notice);
+    }
+
+
+}
---
+++ notice/src/main/java/com/example/notice/service/NoticeService.java
@@ -0,0 +1,21 @@
+package com.example.notice.service;
+
+import com.example.notice.entity.Notice;
+import com.example.notice.repository.NoticeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeService {
+
+    @Autowired
+    private NoticeRepository noticeRepository;
+
+    public Notice write(Notice notice){
+
+        noticeRepository.save(notice);
+        return notice;
+    }
+
+
+}
