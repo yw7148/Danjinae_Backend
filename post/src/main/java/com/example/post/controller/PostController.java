00a1aa4ef83b34b13f078b4db0db74c060d250a8
guswl197 <guswl197@ajou.ac.kr>
2021-11-04 14:51:15 +0900

    post API 수정

--- post/src/main/java/com/example/post/controller/PostController.java
+++ post/src/main/java/com/example/post/controller/PostController.java
@@ -8,10 +8,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class PostController {
 
     @Autowired
@@ -24,15 +23,14 @@ public class PostController {
     }
 
     //게시물 등록
-    @PostMapping("/addpost")
-    public String addPost(Post post){
-        postService.write(post);
-        return "redirect:/totalpostlist";
+    @PutMapping("/addpost")
+    public Post addPost(@RequestBody Post post){
+        return postService.write(post);
     }
 
     //게시물 전체 리스트
-    @GetMapping("/totalpostlist")
-    public String totalPostList(Model model, @PageableDefault(page=0,size=10,sort="id",direction= Sort.Direction.DESC) Pageable pageable,String keyword){
+    @GetMapping("/totalpostlist/{keyword}")
+    public Page<Post> totalPostList(Model model, @PageableDefault(page=0,size=10,sort="id",direction= Sort.Direction.DESC) Pageable pageable,@PathVariable String keyword){
 
         Page<Post> list= null;
 
@@ -51,22 +49,23 @@ public class PostController {
         model.addAttribute("newPage",nowPage);
         model.addAttribute("startPage",startPage);
         model.addAttribute("endPage",endPage);
-        return "totalpostlist";
+        return list;
     }
 
     //선택된 게시물 불러오기
-    @GetMapping("/getpost")
-    public String getPost(Model model,Integer id){
+    @GetMapping("/getpost/{id}")
+    public Post getPost(Model model,@PathVariable Integer id){
 
         model.addAttribute("post",postService.getpost(id));
-        return "getpost";
+        return postService.getpost(id);
     }
 
     //선택한 게시글 삭제
-    @GetMapping("/deletepost")
-    public String deletePost(Integer id){
+    @GetMapping("/deletepost/{id}")
+    public Page<Post> deletePost(@PathVariable Integer id, @PageableDefault(page=0,size=10,sort="id",direction= Sort.Direction.DESC) Pageable pageable){
 
         postService.deletepost(id);
-        return "redirect:/totalpostlist";
+        Page<Post> list= postService.totalpostlist(pageable);
+        return list;
     }
 }
