67b4b50e663f17c18a958b743653e40279794a39
yw7148 <yw7148@ajou.ac.kr>
2021-11-08 21:23:35 +0900

    Swagger관련 오류 수정
    Swagger관련 오류 수정

--- danjinae/src/main/java/com/capstone/danjinae/Configuration/Swagger2Config.java
+++ danjinae/src/main/java/com/capstone/danjinae/Configuration/Swagger2Config.java
@@ -15,6 +15,6 @@ public class Swagger2Config {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.danjinae.rest")).paths(PathSelectors.any()).build();
+                .apis(RequestHandlerSelectors.basePackage("com.capstone.danjinae")).paths(PathSelectors.any()).build();
     }
 }
